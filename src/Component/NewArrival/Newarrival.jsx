40c811669d7c7ed7f6fc844dc12b09d0365dcb7b
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-10 19:21:25 +0600

    new arrival update

--- src/Component/NewArrival/Newarrival.jsx
+++ src/Component/NewArrival/Newarrival.jsx
@@ -2,15 +2,6 @@ import React, { useState } from "react";
 import Card from "../Common/Card";
 import ArrivalData from "../../../NewArrivalData/ArrivalData";
 import Button from "../Common/Button.jsx";
-import Slider from "react-slick";
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-};
 
 const Newarrival = () => {
   const [arrivalData, setarrivalData] = useState(ArrivalData);
@@ -18,19 +9,17 @@ const Newarrival = () => {
     <>
       <div className="py-12">
         <div className="container">
-          <Slider {...settings}>
-            {arrivalData?.map((item) => (
-              <div key={item.id}>
-                <Card
-                  img={item.productImg}
-                  colorVariant={item.color === true ? true : false}
-                  badge={
-                    item.badge ? <Button>{item.productStatus}</Button> : null
-                  }
-                />
-              </div>
-            ))}
-          </Slider>
+          {arrivalData?.map((item) => (
+            <div key={item.id}>
+              <Card
+                img={item.productImg}
+                colorVariant={item.color === true ? true : false}
+                badge={
+                  item.badge ? <Button>{item.productStatus}</Button> : null
+                }
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
